e705bf08430129df8882bbe62c82cc793134c9a1
Andor Kesselman <akmb2@akmb2.local>
2021-10-02 19:55:10 +0530

    updates with work

--- src/rktoolkit/models/pipeline.py
+++ src/rktoolkit/models/pipeline.py
@@ -9,6 +9,8 @@ from .functions import *
 from typing import List, Optional, Callable, TypedDict
 from ..functions.localization_functions import IterableLocalizationFunction
 from ..functions.linkage_functions import SimpleLinkageFunction
+from ..functions.htg_transformers import CorrelationHTGGenerator
+from ..functions.filters import StaticFilter
 
 class RKPipeline(BaseModel):
     '''
@@ -26,8 +28,8 @@ class RKPipeline(BaseModel):
     preprocess_nodes: Optional[List[TreeTransformNode]] = []
     localization_algorithm: Optional[LocalizationFunction] = IterableLocalizationFunction()
     hfe:HierarchicalTransformGraph = CorrelationHTGGenerator(),
-    filter_functions: TypedDict[str, FilterFunction] = {'all': StaticFilter()}
-    linkage_function: TypedDict[str, LinkageFunction] = {'all': SimpleLinkageFunction(threshold=-1)}
+    filters: TypedDict[str, FilterFunction] = {'all': StaticFilter()}
+    linkers: TypedDict[str, LinkageFunction] = {'all': SimpleLinkageFunction(threshold=-1)}
 
     class Config:
         arbitrary_types_allowed = True
@@ -50,7 +52,7 @@ class RKPipeline(BaseModel):
 
         # Step 4: Run through Filter Functions
         gm = GraphMask()
-        for k, f in self.filter_functions.items():
+        for k, f in self.filters.items():
             # get the filter function node
             n1 = hgraph.get_node_by_id(k)
             if n1 is None:
@@ -61,8 +63,18 @@ class RKPipeline(BaseModel):
             if ns:
                 gm.mask_node(n1)
 
+        links = []
+
         # Step 5: Build links
-        links = self.linkage_function.link(hgraph)
+        for k, f in self.linkers.items():
+            # get the filter function node
+            n1 = hgraph.get_node_by_id(k)
+            if n1 is None:
+                raise ValueError("You've mapped a filter function \
+                to a non-existant correspondence. Make sure to link \
+                the filter function to an id fomr the hfe")
+            links.extend(f.link(n1.value))
+
         return RKModel(
             mask = gm,
             hgraph = hgraph,
